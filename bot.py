7add547fbe9093b74a2cb08749b16d2a06f630a4
Matthew Edwards <mce1996@gmail.com>
2025-04-26 10:08:09 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,20 @@
 # bot.py
 import discord
 from discord.ext import commands
+import os
+import mysql.connector
+
+# Fetch connection info from environment
+db_connection = mysql.connector.connect(
+    host=os.getenv("MYSQL_HOST"),
+    port=int(os.getenv("MYSQL_PORT")),
+    database=os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+)
+
+cursor = db_connection.cursor()
+
 
 intents = discord.Intents.default()
 intents.message_content = True  # Needed to read messages
